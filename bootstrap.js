bf6a18823439b1b68b0de850124c92e8fc60f02f
Gavin Sharp <gavin@gavinsharp.com>
2012-03-12 14:18:10 -0700

    MPL license, change pref to searchreset.backup

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 var Cu = Components.utils;
 
 Cu.import("resource://gre/modules/Services.jsm");
@@ -9,7 +13,7 @@ function startup(aData, aReason) {
   function resetPref(prefName) {
     if (Services.prefs.prefHasUserValue(prefName)) {
       var existingValue = Services.prefs.getCharPref(prefName);
-      Services.prefs.setCharPref("prefreset.backup." + prefName, existingValue);
+      Services.prefs.setCharPref("searchreset.backup." + prefName, existingValue);
 
       Services.prefs.clearUserPref(prefName);
     }
