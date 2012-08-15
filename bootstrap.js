55f17f3de0f0d8d1589dcb3d3c0f6b8afb86a2e1
cornelisvl <dvlcornelis@gmail.com>
2012-08-15 13:58:27 +0300

    fixed error alert parameter, locked prefs test
    Notice that I had the wrong order for the alert parameters.
    Took the opportunity to do some testing with prefs locked via mozilla.cfg that prevented finishing properly.
    Added some DEBUG code for testing.
    I think that it is working now.

--- bootstrap.js
+++ bootstrap.js
@@ -3,6 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var Cu = Components.utils;
+var DEBUG = 0;
 
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/AddonManager.jsm");
@@ -11,6 +12,10 @@ function startup(aData, aReason) {
   // Helper function that backs up and then clears a pref, if it has a user-set
   // value.
   function resetPref(prefName) {
+    if (Services.prefs.prefIsLocked(prefName)){
+      if (DEBUG) Services.prompt.alert(null, "RESET", "LOCKED: "+prefName);
+    }
+
     if (Services.prefs.prefHasUserValue(prefName)) {
       var existingValue = Services.prefs.getCharPref(prefName);
       Services.prefs.setCharPref("searchreset.backup." + prefName, existingValue);
@@ -28,11 +33,15 @@ function startup(aData, aReason) {
 
   // Now also reset the default search engine
   resetPref("browser.search.defaultenginename");
-  let originalDefaultEngine = Services.search.originalDefaultEngine;
-  originalDefaultEngine.hidden = false;
-  Services.search.currentEngine = originalDefaultEngine;
-  Services.search.moveEngine(originalDefaultEngine, 0);
 
+  let originalDefaultEngine = Services.search.originalDefaultEngine;
+
+  if (originalDefaultEngine) {
+    originalDefaultEngine.hidden = false;
+    Services.search.currentEngine = originalDefaultEngine;
+    Services.search.moveEngine(originalDefaultEngine, 0);
+  } else if (DEBUG) Services.prompt.alert(null, "ERROR", "NO originalDefaultEngine");
+  
   // Flush changes to disk
   Services.prefs.savePrefFile(null);
 
@@ -40,18 +49,21 @@ function startup(aData, aReason) {
   // code reused from AboutHomeUtils
   // http://mxr.mozilla.org/mozilla-central/source/browser/components/nsBrowserContentHandler.js#838
   let defaultEngine = Services.search.originalDefaultEngine;
-  let submission = defaultEngine.getSubmission("_searchTerms_");
-  let engine = {name: defaultEngine.name, searchUrl: submission.uri.spec};
-  let aboutHomeURI = Services.io.newURI("moz-safe-about:home", null, null);
 
-	try {
-	let ssm = Components.classes["@mozilla.org/scriptsecuritymanager;1"].getService(Components.interfaces.nsIScriptSecurityManager);
-  let principal = (ssm.getCodebasePrincipal || ssm.getNoAppCodebasePrincipal)(aboutHomeURI);
+  if (defaultEngine) {
+    let submission = defaultEngine.getSubmission("_searchTerms_");
+    let engine = {name: defaultEngine.name, searchUrl: submission.uri.spec};
+    let aboutHomeURI = Services.io.newURI("moz-safe-about:home", null, null);
 
-  let dsm = Components.classes["@mozilla.org/dom/storagemanager;1"].getService(Components.interfaces.nsIDOMStorageManager);
+    try {
+      let ssm = Components.classes["@mozilla.org/scriptsecuritymanager;1"].getService(Components.interfaces.nsIScriptSecurityManager);
+      let principal = (ssm.getCodebasePrincipal || ssm.getNoAppCodebasePrincipal)(aboutHomeURI);
 
-	dsm.getLocalStorageForPrincipal(principal, "").setItem("search-engine", JSON.stringify(engine));
-	} catch(e) { Services.prompt.alert(null, e, "Error resetting about:home"); }
+      let dsm = Components.classes["@mozilla.org/dom/storagemanager;1"].getService(Components.interfaces.nsIDOMStorageManager);
+
+      dsm.getLocalStorageForPrincipal(principal, "").setItem("search-engine", JSON.stringify(engine));
+     } catch(e) { if (DEBUG) Services.prompt.alert(null, "ERROR resetting about:home" ,e); }
+  } else if (DEBUG) Services.prompt.alert(null, "ERROR", "NO defaultEngine");
 
   // auto-uninstall after running
   AddonManager.getAddonByID(aData.id, function(addon) {
