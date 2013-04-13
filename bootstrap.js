6eb18c0f8d3199ca9d7f7ea29b4234db1b5dc2d9
Gavin Sharp <gavin@gavinsharp.com>
2013-04-13 12:34:33 -0700

    Add support for FF16-and-later search service initialization, and support resetting the default engine post bug 738818 (FF23 and later). Also add some console logging.

--- bootstrap.js
+++ bootstrap.js
@@ -3,20 +3,28 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var Cu = Components.utils;
+var Ci = Components.interfaces;
 
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/AddonManager.jsm");
+
+function logStatus(m) {
+  Services.console.logStringMessage("SearchReset: " + m);
+}
 
 function startup(aData, aReason) {
   // Helper function that backs up and then clears a pref, if it has a user-set
   // value.
   function resetPref(prefName) {
-    if (Services.prefs.prefHasUserValue(prefName)) {
-      var existingValue = Services.prefs.getCharPref(prefName);
-      Services.prefs.setCharPref("searchreset.backup." + prefName, existingValue);
-
-      Services.prefs.clearUserPref(prefName);
+    if (!Services.prefs.prefHasUserValue(prefName)) {
+      logStatus("No need to reset " + prefName);
+      return;
     }
+
+    logStatus("Resetting " + prefName);
+    var existingValue = Services.prefs.getCharPref(prefName);
+    Services.prefs.setCharPref("searchreset.backup." + prefName, existingValue);
+    Services.prefs.clearUserPref(prefName);
   }
 
   // Reset the home page and keyword.URL
@@ -27,19 +35,61 @@ function startup(aData, aReason) {
   resetPref("browser.newtab.url");
 
   // Now also reset the default search engine
-  resetPref("browser.search.defaultenginename");
-  let originalDefaultEngine = Services.search.originalDefaultEngine;
-  originalDefaultEngine.hidden = false;
-  Services.search.currentEngine = originalDefaultEngine;
-  Services.search.moveEngine(originalDefaultEngine, 0);
+  // To be compatible with all versions before/after bug 738818, get the default
+  // defaultenginename pref value manually.
+  var defEnginePref = "browser.search.defaultenginename";
+  resetPref(defEnginePref);
+  var defaultEngineName;
+  try {
+    defaultEngineName = Services.prefs.getComplexValue(defEnginePref, Ci.nsIPrefLocalizedString).data;
+  } catch (ex) {
+    // This shouldn't happen, but maybe an extension screwed things up?
+    Components.utils.reportError("SearchReset: Unable to get defaultEngineName: " + ex);
+  }
 
-  // Flush changes to disk
-  Services.prefs.savePrefFile(null);
+  if (defaultEngineName) {
+    logStatus("Found default engine name");
+    // Be compatible with versions prior to the creation of Services.search.init (bug 722332, Firefox 16)
+    if (Services.search.init) {
+      logStatus("Initializing search service");
+      Services.search.init(function (result) {
+        if (!Components.isSuccessCode(result)) {
+          Components.utils.reportError("Search service initialization failed: " + result);
+          flushPrefsAndUninstall();
+          return;
+        }
+        resetDefaultEngine();
+        flushPrefsAndUninstall();
+      });
+    } else {
+      // Pre-Firefox 16 (bug 722332)
+      logStatus("Search service doesn't require initialization");
+      resetDefaultEngine();
+      flushPrefsAndUninstall();
+    }
+  } else {
+    flushPrefsAndUninstall();
+  }
 
-  // auto-uninstall after running
-  AddonManager.getAddonByID(aData.id, function(addon) {
-    addon.uninstall();
-  });
+  function resetDefaultEngine() {
+    logStatus("Resetting default engine");
+    var originalDefaultEngine = Services.search.getEngineByName(defaultEngineName);
+    originalDefaultEngine.hidden = false;
+    Services.search.currentEngine = originalDefaultEngine;
+    Services.search.moveEngine(originalDefaultEngine, 0);
+  }
+
+  function flushPrefsAndUninstall() {
+    logStatus("Flushing prefs and uninstalling");
+    // Flush changes to disk
+    Services.prefs.savePrefFile(null);
+
+    // auto-uninstall after running
+    AddonManager.getAddonByID(aData.id, function(addon) {
+      addon.uninstall();
+      logStatus("Addon uninstalled");
+    });
+  }
 }
 
 function shutdown(aData, aReason) { }
